fe9cea52fd0f8b191988671a91f7622a0621164a
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2025-04-14 17:40:55 +0300

    login nenea

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>login</title>
+</head>
+<body>
+    <form action="process_login.php" method="POST">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+        
+        <button type="submit">Login</button>
+    </form>
+    <footer>
+        Copyright 2025 R incercuit
+    </footer>
+</body>
+</html>
