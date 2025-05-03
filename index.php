8afd299a142c72c91dad71b6b4f4ecede3053918
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2025-05-03 17:23:45 +0300

    home page

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styleHome.css">
     <title>Index</title>
 </head>
 <body>
-    <h1>NIGGER</h1>
+    <nav class="navbar">
+        <a href="index.php">Home</a>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>
+    </nav>
 </body>
 </html>
